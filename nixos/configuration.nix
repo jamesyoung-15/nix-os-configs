181eedb0aa9b170cc8e2dc3e9e8b4051fc4d3cae
James <jyyoung@jyymail.com>
2024-10-10 09:25:03 -0400

    remove virtualbox

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -413,13 +413,6 @@
   programs.virt-manager.enable = true;
   services.spice-vdagentd.enable = true;  # enable copy and paste between host and guest
 
-  # enable virtualbox
-  virtualisation.virtualbox.host.enable = true;
-  virtualisation.virtualbox.host.enableExtensionPack = true;
-  virtualisation.virtualbox.guest.enable = true;
-  virtualisation.virtualbox.guest.draganddrop = true;
-  users.extraGroups.vboxusers.members = [ "jamesyoung" ];
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
