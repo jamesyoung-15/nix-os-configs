2f49147711ded724666fe578d0bb94cda60cb267
James <jyyoung@jyymail.com>
2024-10-10 09:12:57 -0400

    add fcitx5

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -90,7 +90,6 @@
   # keyboard
   i18n.inputMethod = {
      enabled = "fcitx5";
-     waylandFrontend = true;
      fcitx5.addons = with pkgs; [
        fcitx5-gtk             # alternatively, kdePackages.fcitx5-qt
        fcitx5-chinese-addons  # table input method support
