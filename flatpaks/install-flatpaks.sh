d78bb4d2cd640922cbd2ebe3cfa6bebcb946b7dc
James <jyyoung@jyymail.com>
2024-11-14 15:15:36 -0500

    add flatpaks

---
+++ flatpaks/install-flatpaks.sh
@@ -0,0 +1,4 @@
+flatpak install flathub com.logseq.Logseq -y
+flatpak install flathub net.cozic.joplin_desktop -y
+flatpak install flathub org.qbittorrent.qBittorrent -y
+flatpak install flathub org.ryujinx.Ryujinx -y
